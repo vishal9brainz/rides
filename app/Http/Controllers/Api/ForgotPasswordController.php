ac74f0b8a95fe5f734fb22b4e0500d60ec30ddee
vishal1995 <vishal.documents1995@gmail.com>
2019-07-24 17:59:21 +0530

    revert chanfes

--- app/Http/Controllers/Api/ForgotPasswordController.php
+++ app/Http/Controllers/Api/ForgotPasswordController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Auth;
 use App\User;
+use mail;
 use App\Http\Controllers\Controller;
 
 class ForgotPasswordController extends Controller
