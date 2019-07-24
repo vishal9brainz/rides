9f5f91aec619db7d4528df25986529f9cd92c58a
vishal1995 <vishal.documents1995@gmail.com>
2019-07-24 18:21:33 +0530

    revert chanfes

--- app/Http/Controllers/Api/ForgotPasswordController.php
+++ app/Http/Controllers/Api/ForgotPasswordController.php
@@ -5,17 +5,15 @@ use App\Http\Requests\ForgotPasswordRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Auth;
-use App\User;
 use App\Http\Controllers\Controller;
 
 class ForgotPasswordController extends Controller
 {
     public function sendEmail(ForgotPasswordRequest $request)
     {
-        $user=User::where('email',$request->email)->first();
-        $token = app('auth.password.broker')->createToken($user);
-        $url='http://samp.ca/rides/password/reset/'.$token;
-    	mail($request->email,'Reset Password Link',$url);
+    	$response = $this->broker()->sendResetLink(
+            $request->only('email')
+        );
         return response()->json([
         	'success'		=>true,
         	'Message'		=>'Mail Sent SuccessFully',
